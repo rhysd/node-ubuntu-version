c77ecc2bad851cd9419c94ad8482d95bb4b9a8ee
rhysd <lin90162@yahoo.co.jp>
2021-02-07 02:58:09 +0900

    run tests on all platforms using mock-require

--- test.ts
+++ test.ts
@@ -1,47 +1,129 @@
 import { strict as A } from 'assert';
-import { execSync } from 'child_process';
-import { getUbuntuVersion } from '.';
+import mock = require('mock-require');
+import { getUbuntuVersion, UbuntuVersion } from '.';
 
-function isUbuntu(): boolean {
-    if (process.platform !== 'linux') {
-        return false;
+class ExecFileMock {
+    called: any[] | null;
+    getUbuntuVersion: () => Promise<UbuntuVersion | null>;
+
+    constructor(public output: string | Error) {
+        this.called = null;
+
+        const execFile = (...args: any[]) => {
+            this.called = args;
+            const cb = args[args.length - 1];
+            if (typeof this.output === 'string') {
+                cb(null, this.output, '');
+            } else {
+                cb(this.output, '', 'error test');
+            }
+        };
+        mock('child_process', { execFile });
+        this.getUbuntuVersion = mock.reRequire('.').getUbuntuVersion;
     }
-    const stdout = execSync('lsb_release -a', { encoding: 'utf8' });
-    return /Distributor ID:\s*Ubuntu/.test(stdout);
+
+    static finalize() {
+        mock.stop('child_process');
+    }
 }
 
 describe('getUbuntuVersion()', function () {
-    if (isUbuntu()) {
-        it('should return Ubuntu version info for Ubuntu', async function () {
-            const ret = await getUbuntuVersion();
-            A.ok(ret);
+    const savedPlatform = process.platform;
 
-            const { description, release, codename, version } = ret;
-            const msg = JSON.stringify({ description, release, codename, version });
-            A.ok(description, msg);
-            A.ok(release, msg);
-            A.ok(codename, msg);
-            A.ok(version.length >= 2, msg);
-
-            A.ok(description.includes('Ubuntu'), msg); // e.g. "Ubuntu 18.04 LTS"
-            A.ok(/^\d+\.\d+$/.test(release), msg); // e.g. "18.04"
-            A.ok(description.includes(release), msg);
-            A.ok(/^[a-zA-Z]+$/.test(codename), msg); // e.g. "bionic"
-            const verString = `${version[0]}.${version[1].toString().padStart(2, '0')}`;
-            A.ok(description.includes(verString));
-            A.ok(release.startsWith(verString));
-        });
-    }
-
-    context('on OS other than Ubuntu', function () {
-        let saved: string;
-
+    context('on Ubuntu', function () {
         before(function () {
-            saved = process.platform;
+            Object.defineProperty(process, 'platform', { value: 'linux' });
         });
 
         after(function () {
-            Object.defineProperty(process, 'platform', { value: saved });
+            Object.defineProperty(process, 'platform', { value: savedPlatform });
+            ExecFileMock.finalize();
+        });
+
+        it('parses Ubuntu 20.04 lsb_release output', async function () {
+            const output = [
+                'No LSB modules are available.',
+                'Distributor ID:	Ubuntu',
+                'Description:	Ubuntu 20.04.2 LTS',
+                'Release:	20.04',
+                'Codename:	focal',
+            ].join('\n');
+            const mocked = new ExecFileMock(output);
+            const ret = await mocked.getUbuntuVersion();
+            A.ok(ret);
+            A.equal(ret.description, 'Ubuntu 20.04.2 LTS');
+            A.equal(ret.release, '20.04');
+            A.equal(ret.codename, 'focal');
+            A.deepEqual(ret.version, [20, 4, 2]);
+            A.ok(mocked.called);
+            A.equal(mocked.called[0], 'lsb_release');
+        });
+
+        it('parses Ubuntu 20.10 lsb_release output', async function () {
+            const output = [
+                'No LSB modules are available.',
+                'Distributor ID:	Ubuntu',
+                'Description:	Ubuntu 20.10.1',
+                'Release:	20.10',
+                'Codename:	groovy',
+            ].join('\n');
+            const mocked = new ExecFileMock(output);
+            const ret = await mocked.getUbuntuVersion();
+            A.ok(ret);
+            A.equal(ret.description, 'Ubuntu 20.10.1');
+            A.equal(ret.release, '20.10');
+            A.equal(ret.codename, 'groovy');
+            A.deepEqual(ret.version, [20, 10, 1]);
+        });
+
+        it('returns null with no distributor ID', async function () {
+            const output = ['No LSB modules are available.'].join('\n');
+            const mocked = new ExecFileMock(output);
+            const ret = await mocked.getUbuntuVersion();
+            A.equal(ret, null);
+        });
+
+        it('returns null when lsb_release is not found', async function () {
+            // Pseudo SystemError since no constructor is public for the class
+            const err = Object.assign(new Error(''), { errno: -2, code: 'ENOENT' });
+            const mocked = new ExecFileMock(err);
+            const ret = await mocked.getUbuntuVersion();
+            A.equal(ret, null);
+        });
+
+        it('returns null on Linux other than Ubuntu', async function () {
+            const output = [
+                'LSB Version:	:base-4.0-ia32:base-4.0-noarch:core-4.0-ia32:core-4.0-noarch',
+                'Distributor ID:	CentOS',
+                'Description:	CentOS release 6.7 (Final)',
+                'Release:	6.7',
+                'Codename:	Final',
+            ].join('\n');
+            const mocked = new ExecFileMock(output);
+            const ret = await mocked.getUbuntuVersion();
+            A.equal(ret, null);
+        });
+
+        it('throws an error when lsb_release command fails', async function () {
+            const err = new Error('this is error message');
+            const mocked = new ExecFileMock(err);
+            await A.rejects(
+                async () => {
+                    await mocked.getUbuntuVersion();
+                },
+                err => {
+                    A.ok(err.message.startsWith('Could not execute'));
+                    A.ok(err.message.includes('this is error message'));
+                    A.ok(err.message.includes('error test'));
+                    return true;
+                },
+            );
+        });
+    });
+
+    context('on non-Linux OSes', function () {
+        after(function () {
+            Object.defineProperty(process, 'platform', { value: savedPlatform });
         });
 
         for (const platform of ['darwin', 'windows']) {
