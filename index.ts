6936354fce90e1bf9ed587bd71a426550cfc6a2e
rhysd <lin90162@yahoo.co.jp>
2021-02-27 20:36:47 +0900

    change getUbuntuVersion() to return an array of versions

--- index.ts
+++ index.ts
@@ -1,31 +1,5 @@
 import { execFile } from 'child_process';
 
-export interface UbuntuVersion {
-    readonly description: string;
-    readonly release: string;
-    readonly codename: string;
-    readonly version: number[];
-}
-
-class LsbReleaseOutput implements UbuntuVersion {
-    constructor(public description: string, public release: string, public codename: string) {}
-
-    get version(): number[] {
-        const re = /(\d+)\.(\d+)(?:\.(\d+))?/;
-        for (const s of [this.description, this.release]) {
-            const m = s.match(re);
-            if (m !== null) {
-                const ss = [m[1], m[2]];
-                if (m[3]) {
-                    ss.push(m[3]);
-                }
-                return ss.map(s => parseInt(s, 10));
-            }
-        }
-        return [];
-    }
-}
-
 function isSystemError(e: Error): e is NodeJS.ErrnoException {
     return 'errno' in e;
 }
@@ -46,51 +20,59 @@ function command(exe: string, args: string[]): Promise<string | null> {
     });
 }
 
-export async function getUbuntuVersion(): Promise<UbuntuVersion | null> {
+export async function getUbuntuVersion(): Promise<number[]> {
     if (process.platform !== 'linux') {
-        return null;
+        return [];
     }
 
     const stdout = await command('lsb_release', ['-a']);
     if (stdout === null) {
-        return null;
+        return [];
     }
 
     const reDistributor = /^Distributor ID:\s*(.+)$/;
-    const reDescription = /^Description:\s*(.+)$/;
-    const reRelease = /^Release:\s*(.+)$/;
-    const reCodename = /^Codename:\s*(.+)$/;
+    const reDescription = /^Description:\s*Ubuntu\s+(\d+)\.(\d+)(?:\.(\d+))?/;
+    const reRelease = /^Release:\s*(\d+)\.(\d+)(?:\.(\d+))?$/;
     let description = null;
     let release = null;
-    let codename = null;
     let distributorFound = false;
     for (const line of stdout.split('\n')) {
         const m = line.match(reDistributor);
         if (m !== null) {
             const distributor = m[1];
             if (distributor !== 'Ubuntu') {
-                return null;
+                return [];
             }
             distributorFound = true;
         }
 
-        const desc = line.match(reDescription)?.[1];
+        const desc = line.match(reDescription);
         if (desc) {
             description = desc;
         }
-        const rel = line.match(reRelease)?.[1];
+        const rel = line.match(reRelease);
         if (rel) {
             release = rel;
         }
-        const code = line.match(reCodename)?.[1];
-        if (code) {
-            codename = code;
+
+        if (distributorFound && description && release) {
+            break;
         }
     }
 
-    if (!distributorFound || description === null || release === null || codename === null) {
-        return null;
+    if (!distributorFound) {
+        return [];
     }
 
-    return new LsbReleaseOutput(description, release, codename);
+    for (const m of [description, release]) {
+        if (m) {
+            const ss = [m[1], m[2]];
+            if (m[3]) {
+                ss.push(m[3]);
+            }
+            return ss.map(s => parseInt(s, 10));
+        }
+    }
+
+    return [];
 }
